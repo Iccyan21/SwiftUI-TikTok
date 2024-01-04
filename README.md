d2be3ae312a94291e0d51e7d0e8ef6f02b20359c
Iccyan21 <iccyan0429@gmail.com>
2024-01-04 17:42:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SwiftUI-TikTok
+# SwiftUI-TikTok
---
+++ SwiftUI-TikTok/SwiftUI_TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TikTokApp.swift
+//  SwiftUI-TikTok
+//
+//  Created by 水原　樹 on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
